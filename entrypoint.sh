dd08064a5a33a3aa8cd5a49ac6aa709c3b842c22
JirayuSrisawat-Github <contact@jirayu.pw>
2023-06-03 19:55:59 +0700

    commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+echo "Made with Love from Oxygen-Project"
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
